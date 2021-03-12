d568e45868b4f7213825cb642680460fe4a32589
Judit <onilne@bubblegum.agency>
2021-03-12 12:45:40 +0100

    ocultar tabla facturas si no es user con priv 1

--- index.php
+++ index.php
@@ -477,12 +477,7 @@ if(isset($_SESSION['priv']) && $_SESSION['priv'] == 1):
             </div>
         </div>
     </div> <!-- /listado -->
-<?php
-endif;
-?>
-<?php
-if(isset($_SESSION['priv']) && $_SESSION['priv'] == 1):
-?>
+
     <div class="listado">
         <div class="row">
             <div class="col-md-10">
